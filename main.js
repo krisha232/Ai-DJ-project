2708ab7404dddc1f83cef9bae9203dfe34df6dce
krisha232 <78255450+krisha232@users.noreply.github.com>
2021-11-29 16:41:37 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song1 = "";
 song2 = "";
+leftx=0;
+lefty=0;
+rightx=0;
+righty=0;
 
 function setup() {
     canvas = createCanvas(600, 500);
@@ -8,7 +12,27 @@ function setup() {
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
 }
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        leftx=results[0].pose.leftWrist.x;
+        lefty=results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftx + "LeftWristY = " + lefty);
+
+        rightx=results[0].pose.rightWrist.x;
+        righty=results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightx + "RightWristY = " + righty);
+}}
+
+function modelLoaded(){
+    console.log('PoseNet is initialized');
+}
+
 
 function draw() {
     image(video, 0, 0, 600, 500);
